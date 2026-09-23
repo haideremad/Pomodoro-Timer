9bbb0dbc1a311819a1aa29f06bc4a047d97c4373
haideremad <haider.emad.abdulzahra@gmail.com>
2026-09-23 18:45:17 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Pomodoro__Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.btnStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Image = global::Pomodoro__Project.Properties.Resources.Pomodoro;
+            this.pictureBox1.Location = new System.Drawing.Point(-7, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(757, 452);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(70, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 69);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "25 : 00";
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnStart.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(50, 258);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(186, 62);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start Focus";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.BalloonTipClicked += new System.EventHandler(this.notifyIcon1_BalloonTipClicked);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnStop.Font = new System.Drawing.Font("Bookman Old Style", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(50, 352);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(186, 62);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(748, 450);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = " برنامج بومودورو";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pomodoro__Project
+{
+    public partial class Form1 : Form
+    {
+        private int Minutes = 0;
+        private int Seconds = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        enum enStatus {Focus = 0,Break = 1};
+
+        enStatus Status = enStatus.Focus;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+          
+            if (Seconds == 0)
+            {
+                if (Minutes == 0)
+                {
+                    timer1.Enabled = false;
+                    if(Status == enStatus.Focus)
+                    {
+                        notifyIcon1.BalloonTipTitle = "انتهت جلسة التركيز";
+                        notifyIcon1.BalloonTipText = "حان وقت أخذ استراحة قصيرة (5 دقائق)";
+                        Status = enStatus.Break;
+                        btnStart.Text = "Start Break";
+                        label1.Text = "5 : 0";
+                        btnStart.Enabled = true;
+                    }
+
+                    else
+                    {
+                        notifyIcon1.BalloonTipTitle = "انتهت جلسة الإستراحة";
+                        notifyIcon1.BalloonTipText = "حان وقت التركيز (25 دقيقة)";
+                        Status = enStatus.Focus;
+                        btnStart.Text = "Start Focus";
+                        label1.Text = "25 : 0";
+                        btnStart.Enabled = true;
+                    }
+
+                    notifyIcon1.Icon = SystemIcons.Application;
+                    notifyIcon1.Text = "Pomodoro Program";
+                    notifyIcon1.ShowBalloonTip(1000);
+                  
+                    btnStart.Enabled = true; 
+                }
+                else
+                {
+                    Minutes--;
+                    Seconds = 59;
+                    label1.Text = Minutes.ToString() + " : " + Seconds.ToString();
+                }
+            }
+
+            else
+            {
+                Seconds--;
+                label1.Text = Minutes.ToString() + " : " + Seconds.ToString();
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            if (Status == enStatus.Focus)
+            {
+                Minutes = 25;
+                Seconds = 0;
+            }
+
+            else
+            {
+                Minutes = 5;
+                Seconds = 0;
+            }
+            timer1.Enabled = true;
+        }
+
+        private void notifyIcon1_BalloonTipClicked(object sender, EventArgs e)
+        {
+            if(this.WindowState == FormWindowState.Minimized)
+                this.WindowState = FormWindowState.Normal;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if(timer1.Enabled == true)
+                timer1.Enabled = false;
+            else if (Minutes != 0 || Seconds != 0)
+                timer1.Enabled = true;
+        }
+
+
+    }
+}
